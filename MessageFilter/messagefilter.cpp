70a03566a41f91a7a058a6258308f022acca7d7e
CamperGit <sashchenkodmitrii@mail.ru>
2021-06-23 15:48:42 +0300

    fix message filtering by time

--- MessageFilter/messagefilter.cpp
+++ MessageFilter/messagefilter.cpp
@@ -5,8 +5,8 @@ void MessageFilter::filterByTime(QVector<Message>& messages)
     for (int i = 0; i < messages.size() - 1; i++)
     {
         for (int j = 0; j < messages.size() - i - 1; j++)
-        {
-            if (messages[j].getSendTime().currentSecsSinceEpoch() < messages[j + 1].getSendTime().currentSecsSinceEpoch())
+        {        
+            if (messages[j].getSendTime() >= messages[j + 1].getSendTime())
             {
                 Message temp = messages[j];
                 messages[j] = messages[j + 1];
--- tests/main.cpp
+++ tests/main.cpp
@@ -8,29 +8,34 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QDate date1(2021,6,23);
+    QDate date(2021,6,23);
     QTime time1(23,16);
-    QDate date2(2021,6,23);
-    QTime time2(13,16);
-    QDateTime sendDateTime1(date1,time1);
-    QDateTime sendDateTime2(date2,time2);
-    Message mes1("text",sendDateTime1,"subject","sender","receiver");
-    Message mes2("text2",sendDateTime2,"subject2","sender2","receiver2");
+    QTime time2(13,15);
+    QTime time3(12,13);
+    QTime time4(18,12);
+    QDateTime sendDateTime1(date,time1);
+    QDateTime sendDateTime2(date,time2);
+    QDateTime sendDateTime3(date,time3);
+    QDateTime sendDateTime4(date,time4);
+    Message mes1("1",sendDateTime1,"subject","sender","receiver");
+    Message mes2("2",sendDateTime2,"subject2","sender2","receiver2");
+    Message mes3("3",sendDateTime3,"subject3","sender3","receiver3"); // 2 1 3 4
+    Message mes4("4",sendDateTime4,"subject4","sender4","receiver4"); //3 2 4 1
     QVector<Message> messages;
     messages.push_back(mes2);
     messages.push_back(mes1);
+    messages.push_back(mes3);
+    messages.push_back(mes4);
     for(int i = 0; i < messages.size(); i++)
     {
         qDebug() << messages[i].getText();
     }
+    qDebug() << "\n\n";
     MessageFilter::filterByTime(messages);
     for(int i = 0; i < messages.size(); i++)
     {
         qDebug() << messages[i].getText();
     }
-    //QString val = filteredMessages[0].getText();
-
-    //qDebug() << val;
 
     return a.exec();
 }
