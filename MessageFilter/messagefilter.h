de8b962ad9bff18ed0383cfbeda199c4b397e6f4
CamperGit <sashchenkodmitrii@mail.ru>
2021-06-23 14:16:41 +0300

    start working on users loading

--- MessageFilter/messagefilter.h
+++ MessageFilter/messagefilter.h
@@ -9,7 +9,6 @@
 class MESSAGEFILTER_EXPORT MessageFilter
 {
 public:
-    static QVector<Message> getMessagesSentBeforeSelectedTime(QVector<Message> messages, QDateTime timeBefore);
     static QVector<Message> filterByTime(QVector<Message> messages, QDateTime date);
 };
 
