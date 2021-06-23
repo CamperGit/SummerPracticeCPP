301725176fafb43280c3c32427527ead02f6d619
imreallykind <dimaelfimov228@gmail.com>
2021-06-23 12:49:40 +0300

    git test

--- Message/message.h
+++ Message/message.h
@@ -28,6 +28,7 @@ public:
     void setSubject(const QString subject);
     void setSenderLogin(const QString senderLogin);
     void setReceiverLogin(const QString receiverLogin);
+    void setMessageTime();
 };
 
 #endif // MESSAGE_H
