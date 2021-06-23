1c9357c3ed8e85eb89aec78763465d0c614d6d07
imreallykind <dimaelfimov228@gmail.com>
2021-06-23 13:51:19 +0300

    new filtering

--- MessageFilter/messagefilter.cpp
+++ MessageFilter/messagefilter.cpp
@@ -1,11 +1,11 @@
 #include "messagefilter.h"
 
-QVector<Message> MessageFilter::getMessagesSentBeforeSelectedTime(QVector<Message> messages, QDateTime timeBefore)
+QVector<Message> MessageFilter::filterByDateAndTime(QVector<Message> messages)
 {
     QVector<Message> filteredMessages;
     for (int i = 0 ; i < messages.size();i++)
     {
-        if (messages[i].getSendTime().currentSecsSinceEpoch() < timeBefore.currentSecsSinceEpoch())
+        if (messages[i].getSendTime().currentSecsSinceEpoch() < messages[i+1].getSendTime().currentSecsSinceEpoch())
         {
             filteredMessages.push_back(messages[i]);
         }
