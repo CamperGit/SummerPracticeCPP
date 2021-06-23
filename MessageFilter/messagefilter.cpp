1566606890ae15d0f3a0ced80496a8dab3b8e0a9
imreallykind <dimaelfimov228@gmail.com>
2021-06-23 17:06:15 +0300

    filtering by name

--- MessageFilter/messagefilter.cpp
+++ MessageFilter/messagefilter.cpp
@@ -1,4 +1,9 @@
 #include "messagefilter.h"
+#include <QString>
+/*
+* Filters sent messages by sending time.
+* @param QVector<Message> messages - messages to filter
+*/
 
 void MessageFilter::filterByTime(QVector<Message>& messages)
 {
@@ -11,6 +16,27 @@ void MessageFilter::filterByTime(QVector<Message>& messages)
                 Message temp = messages[j];
                 messages[j] = messages[j + 1];
                 messages[j + 1] = temp;
+            }
+        }
+    }
+}
+
+/*
+* Filters sent messages by alphabet.
+* @param QVector<Message> messages - messages to filter
+*/
+
+void MessageFilter::filterByAlphabet(QVector<Message> &messages)
+{
+    for(int i=0; i< messages.size() - 1; i++)
+    {
+        for(int j=0; j < messages.size()-i-1; j++)
+        {
+            if(QString::compare(messages[j].getSubject(),messages[j+1].getSubject(),Qt::CaseInsensitive)>0)
+            {
+            Message tempSubject = messages[j];
+            messages[j] = messages[j+1];
+            messages[j+1] = tempSubject;
             }
         }
     }
--- tests/main.cpp
+++ tests/main.cpp
@@ -17,24 +17,34 @@ int main(int argc, char *argv[])
     QDateTime sendDateTime2(date,time2);
     QDateTime sendDateTime3(date,time3);
     QDateTime sendDateTime4(date,time4);
-    Message mes1("1",sendDateTime1,"subject","sender","receiver");
-    Message mes2("2",sendDateTime2,"subject2","sender2","receiver2");
-    Message mes3("3",sendDateTime3,"subject3","sender3","receiver3"); // 2 1 3 4
-    Message mes4("4",sendDateTime4,"subject4","sender4","receiver4"); //3 2 4 1
+    Message mes1("1",sendDateTime1,"a","sender","receiver");
+    Message mes2("2",sendDateTime2,"b","sender2","receiver2");
+    Message mes3("3",sendDateTime3,"c","sender3","receiver3"); // 2 1 3 4
+    Message mes4("4",sendDateTime4,"d","sender4","receiver4"); //3 2 4 1
     QVector<Message> messages;
     messages.push_back(mes2);
     messages.push_back(mes1);
     messages.push_back(mes3);
     messages.push_back(mes4);
+   //for(int i = 0; i < messages.size(); i++)
+   //{
+   //    qDebug() << messages[i].getText();
+   //}
+   //qDebug() << "\n\n";
+   //MessageFilter::filterByTime(messages);
+   //for(int i = 0; i < messages.size(); i++)
+   //{
+   //    qDebug() << messages[i].getText();
+   //}
     for(int i = 0; i < messages.size(); i++)
     {
-        qDebug() << messages[i].getText();
+        qDebug() << messages[i].getSubject();
     }
     qDebug() << "\n\n";
-    MessageFilter::filterByTime(messages);
+    MessageFilter::filterByAlphabet(messages);
     for(int i = 0; i < messages.size(); i++)
     {
-        qDebug() << messages[i].getText();
+        qDebug() << messages[i].getSubject();
     }
 
     return a.exec();
